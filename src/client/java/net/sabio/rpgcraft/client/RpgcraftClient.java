8e9fc5099ee2d63bc265f297cdc4a465a6b6f5a8
Sabio <gamester.sad@gmail.com>
2026-04-03 19:49:09 +0100

    Initial Commit

---
+++ src/client/java/net/sabio/rpgcraft/client/RpgcraftClient.java
@@ -0,0 +1,10 @@
+package net.sabio.rpgcraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RpgcraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/sabio/rpgcraft/Rpgcraft.java
@@ -0,0 +1,10 @@
+package net.sabio.rpgcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Rpgcraft implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
